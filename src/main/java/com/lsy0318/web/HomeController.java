5321777f57125df4c4055591bc623b545aa0486a
camellia <camellia@DESKTOP-0ANCEPH>
2018-07-24 01:45:36 +0900

    Controller / index 추가 등

--- src/main/java/com/lsy0318/web/HomeController.java
+++ src/main/java/com/lsy0318/web/HomeController.java
@@ -1,6 +1,7 @@
 package com.lsy0318.web;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,13 +22,28 @@ public class HomeController {
 	@Inject
 	private TimeDAO dao;
 
-	@GetMapping("/")
+	@GetMapping("/getTime")
 	public String home(Model model) {
 		logger.info("Welcome home!");
 
 		model.addAttribute("serverTime", dao.getTime());
 
 		return "home";
+	}
+	
+	@GetMapping("/")
+	public String index(HttpServletRequest request) {
+		logger.debug("###INDEX PAGE###");
+
+		String rtnPage = "index";
+		String ipAddress = request.getHeader("X-FORWARDED-FOR");
+		if (ipAddress == null) {
+			ipAddress = request.getRemoteAddr();
+		}
+
+		logger.info(ipAddress + " : " + rtnPage);
+
+		return rtnPage;
 	}
 
 }
